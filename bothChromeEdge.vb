7cb6c0e777a4f8d25e1d5a5f5d9850fdd5fe2b08
Bardi <79906840+Fisny@users.noreply.github.com>
2022-04-19 17:25:32 +0200

    Add files via upload

---
+++ bothChromeEdge.vb
@@ -0,0 +1,58 @@
+﻿Imports OpenQA.Selenium
+Imports OpenQA.Selenium.Chrome
+Imports OpenQA.Selenium.Edge
+Imports OpenQA.Selenium.Support.UI
+Imports OpenQA.Selenium.Chromium
+
+Module bothChromeEdge
+
+    'CHROME & EDGE AUTOMATION'
+    'REFERENCES = NUGETS > NO EXTERNAL DOWNLOADS'
+    'TEST > PROPERTIES > STARTUP OBJECT > BOTH'
+
+    Sub Main()
+        Call Chrome()
+        Call Edge()
+    End Sub
+
+    'CHROME'
+    Sub Chrome()
+        Dim driver As IWebDriver
+        driver = New ChromeDriver
+        driver.Navigate().GoToUrl("http://google.com")
+
+        System.Threading.Thread.Sleep(1500)
+        driver.FindElement(By.Id("L2AGLb")).Click()
+        System.Threading.Thread.Sleep(1500)
+        Dim element As IWebElement = driver.FindElement(By.ClassName("gLFyf"))
+        System.Threading.Thread.Sleep(500)
+        element.SendKeys("Adiacent")
+        System.Threading.Thread.Sleep(2000)
+        driver.FindElement(By.ClassName("gNO89b")).Click()
+        System.Threading.Thread.Sleep(2500)
+
+        ''''''''''''''''''''''''''''''''''''''''''
+    End Sub
+
+    'EDGE'
+    Sub Edge()
+
+        Dim driver As IWebDriver
+        driver = New EdgeDriver
+        driver.Navigate().GoToUrl("http://google.com")
+
+        System.Threading.Thread.Sleep(1500)
+        driver.FindElement(By.Id("L2AGLb")).Click()
+        System.Threading.Thread.Sleep(1500)
+        Dim element As IWebElement = driver.FindElement(By.ClassName("gLFyf"))
+        System.Threading.Thread.Sleep(500)
+        element.SendKeys("Adiacent")
+        System.Threading.Thread.Sleep(2000)
+        driver.FindElement(By.ClassName("gNO89b")).Click()
+        System.Threading.Thread.Sleep(2500)
+    End Sub
+
+
+
+
+End Module
