2f9ce874980efde471a6cbe6b730bfd38bff2371
Bardi <79906840+Fisny@users.noreply.github.com>
2022-04-19 15:30:34 +0200

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,42 @@
+﻿Imports OpenQA.Selenium
+Imports OpenQA.Selenium.Chrome
+Imports OpenQA.Selenium.Support.UI
+
+
+
+
+
+Module Module1
+
+    'GOOGLE CHROME AUTOMATION'
+    Sub Main()
+
+        Dim driver As IWebDriver
+        driver = New ChromeDriver
+        driver.Navigate().GoToUrl("http://google.com")
+
+
+        'Dim element As IWebElement = driver.FindElement(By.Id("L2AGLb"))
+        'System.Threading.Thread.Sleep(1500)
+        'element.Click()
+        'System.Threading.Thread.Sleep(5500)
+
+
+        System.Threading.Thread.Sleep(1500)
+        driver.FindElement(By.Id("L2AGLb")).Click()
+        System.Threading.Thread.Sleep(1500)
+        Dim element As IWebElement = driver.FindElement(By.ClassName("gLFyf"))
+        System.Threading.Thread.Sleep(500)
+        element.SendKeys("Adiacent")
+        System.Threading.Thread.Sleep(2000)
+        driver.FindElement(By.ClassName("gNO89b")).Click()
+        System.Threading.Thread.Sleep(2500)
+
+        ''''''''''''''''''''''''''''''''''''''''''
+
+
+    End Sub
+
+
+
+End Module
